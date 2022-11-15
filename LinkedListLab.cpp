bd1ff22b3590262c5c15a9936be73f050579eb94
Test Student <cuteststudent@billkuker.com>
2022-11-15 07:35:33 -0500

    More comments

--- LinkedListLab.cpp
+++ LinkedListLab.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
 #include <string>
 using std::cout;
-using std::string;
 using std::endl;
+using std::string;
 
 struct List {
-  string value;
-  List *next;
+    string value;
+    List *next;
 };
 
 // Prints the list to cout
 void print(List *l) {
-  while (l != NULL) {
-    // As long as l is not null....
-    cout << l->value << endl; // Print out it's value
-    l = l->next;              // Move on to the next value
-  }
+    while (l != NULL) {
+        // As long as l is not null....
+        cout << l->value;  // Print out it's value
+        l = l->next;       // Move on to the next value
+    }
 }
 
 // Returns the number of items in the list
 int length(List *l) {
-  int length = 0;
-  while (l != NULL) {
-    l = l->next;
-    length++;
-  }
-  return length;
+    int length = 0;
+    while (l != NULL) {
+        l = l->next;
+        length++;
+    }
+    return length;
 }
 
-//Add an item to the list
-void insertAt(List* &list, int pos, string value){
-    if ( list == NULL || pos == 0 ){
+// Add an item to the list
+void insertAt(List *&list, int pos, string value) {
+    if (list == NULL || pos == 0) {
         list = new List{value, list};
     } else {
-        List* l = list;
-        while( l->next != NULL && pos > 1 ){
+        List *l = list;
+        while (l->next != NULL && pos > 1) {
             pos--;
             l = l->next;
         }
@@ -43,11 +43,24 @@ void insertAt(List* &list, int pos, string value){
 }
 
 int main() {
-  List *theList = NULL;
-  insertAt(theList, 100, "A");
-  insertAt(theList, 100, "B");
-  insertAt(theList, 100, "C");
-  insertAt(theList, 100, "D");
-  print(theList);
-  cout << "The list is " << length(theList) << " long." << endl;
+    List *theList = NULL;
+
+    // Each insertAt 0 is like prepend
+    insertAt(theList, 0, "D");
+    insertAt(theList, 0, "C");
+    insertAt(theList, 0, "A");  // List has ACD
+
+    //insertAt 1 will put B after A
+    insertAt(theList, 1, "B");  // Now list has ABCD
+
+    //If the position is bigger than the list it should just be at the end
+    insertAt(theList, 100, "E");  // Now list has ABCDE
+
+    //Expected Output:
+    //  ABCDE
+    //  The list is 5 long.
+    print(theList);
+    cout << endl << "The list is " << length(theList) << " long." << endl;
+
+
 }
